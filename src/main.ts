ad71ce4afc8fbb400b8ae3de3788e3227ddc91ae
myericwang <myericwang02@gmail.com>
2022-01-13 01:27:36 +0800

    delete node module

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+const vm = createApp({
+    data(){
+        return{
+            ischeck: true
+        }
+    }
+})
+
+const a = vm.mount('#section2');
